007ac04b98c6aa3e37382cc269c4327f4a0e140f
Nils Wloka <nils.wloka@codecentric.de>
2014-03-18 11:21:53 +0100

    Routing mit Compojure hinzugefügt.

--- src/bwertr/web/routes.clj
+++ src/bwertr/web/routes.clj
@@ -1,6 +1,11 @@
-(ns bwertr.web.routes)
+(ns bwertr.web.routes
+  (:require [compojure.core :refer [defroutes GET]]))
 
-(defn app [request]
+(defn welcome [request]
   {:status 200
    :headers {"ContentType" "text/html"}
    :body "<h1>Willkommen auf der JavaLand 2014!</h1>"})
+
+(defroutes app
+  (GET "/" [] welcome)
+  (GET "/results" [] "<h1>Results</h1>"))
