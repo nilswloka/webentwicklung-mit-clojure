132a584333e122d9b1dc314d01b12b0188db5009
Nils Wloka <nils.wloka@codecentric.de>
2014-03-18 11:08:54 +0100

    Ring Handler getestet und implementiert.

--- src/bwertr/web/routes.clj
+++ src/bwertr/web/routes.clj
@@ -1,1 +1,6 @@
 (ns bwertr.web.routes)
+
+(defn app [request]
+  {:status 200
+   :headers {"ContentType" "text/html"}
+   :body "<h1>Willkommen auf der JavaLand 2014!</h1>"})
--- test/bwertr/web/routes_tests.clj
+++ test/bwertr/web/routes_tests.clj
@@ -1,2 +1,9 @@
 (ns bwertr.web.routes-tests
-  (:require [bwertr.web.routes :refer :all]))
+  (:require [bwertr.web.routes :refer :all]
+            [clojure.test :refer :all]
+            [ring.mock.request :refer [request]]))
+
+(deftest root-route-shows-welcome-page
+  (let [get-root (request :get "/")
+        response-body (-> get-root app :body)]
+    (is (.contains response-body "Willkommen auf der JavaLand 2014!"))))
